140c963a13c10d60a3d45f073e2b49968774bdf6
James Mitofsky <JamesMitofsky@gmail.com>
2023-09-20 11:53:05 +0200

    IT WORKS — little missiles getting ready to shoot space invaders

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 
+type Missiles = {
+  x: number
+  y: number
+}[]
+
+type Player = {
+  x: number
+  y: number
+  width: number
+  height: number
+  color: string
+  speed: number
+  missiles: Missiles
+}
+
 let oldTimeStamp = 0
 let spaceShip: HTMLImageElement | null = null
 let context: CanvasRenderingContext2D | null = null
 let pressedKey: string | null = null
 const height = 600
 const width = 400
-// let lastPressedKey = null;
 let paused = false
 
-let player = {
+const player: Player = {
   x: 0,
   y: height - 100,
   width: 0,
   height: 0,
   color: 'red',
-  speed: 0.5
+  speed: 0.5,
+  missiles: []
 }
 
 // let player2 = {
@@ -41,7 +56,7 @@ const initGame = () => {
   window.requestAnimationFrame(gameLoop)
 }
 
-const globallyAssignPressedKey = (event: KeyboardEvent) => {
+function managePause(event: KeyboardEvent) {
   const key = event.key
   const currentKeyIsP = key === 'p'
   pressedKey = key
@@ -56,7 +71,16 @@ const globallyAssignPressedKey = (event: KeyboardEvent) => {
     pressedKey = null
     paused = false
   }
-  // console.log(pressedKey);
+}
+
+const globallyAssignPressedKey = (event: KeyboardEvent) => {
+  const currentlyPressedKey = event.key
+
+  // update stored pressed key
+  pressedKey = currentlyPressedKey
+  // if(currentlyPressedKey === "space") {
+
+  // }
 }
 
 const setArrowKeysNull = () => {
@@ -66,13 +90,51 @@ const setArrowKeysNull = () => {
   // console.log(pressedKey);
 }
 
+function drawSpaceship(context: CanvasRenderingContext2D, spaceship: HTMLImageElement) {
+  context.drawImage(spaceship, player.x, player.y)
+}
+
+function drawMissiles(context: CanvasRenderingContext2D) {
+  // draw missiles from the array of missiles in player
+  // for each missile in player.missiles
+  // draw the missile
+  // move the missile
+  player.missiles.forEach((missile) => {
+    context.fillStyle = 'yellow'
+    context.fillRect(missile.x, missile.y, 10, 10)
+  })
+
+  // create bullet
+  // const spaceshipCenterPositionX = player.x + player.width / 2
+  // const spaceshipTopPositionY = player.y
+  // const bulletSize = 10
+  // // set bullet positions so they are centered
+  // const bulletCenterPositionX = spaceshipCenterPositionX - bulletSize / 2
+  // const bulletTopPositionY = spaceshipTopPositionY - bulletSize / 2
+  // context.fillStyle = 'yellow'
+  // context.fillRect(bulletCenterPositionX, bulletTopPositionY, bulletSize, bulletSize)
+
+  // // move the bullet
+  // const bulletSpeed = 1
+}
+
+function addMissileToPlayer() {
+  const bulletSize = 10
+  player.missiles.push({
+    x: player.x + player.width / 2 - bulletSize / 2,
+    y: player.y - bulletSize / 2
+  })
+}
+
 const draw = () => {
+  // error checking
   if (!context || !spaceShip) {
     console.error("Can't find the context or the spaceship:", context, spaceShip)
     return
   }
+  drawSpaceship(context, spaceShip)
 
-  context.drawImage(spaceShip, player.x, player.y)
+  drawMissiles(context)
 }
 
 const gameLoop = (timeStamp: number) => {
@@ -83,17 +145,10 @@ const gameLoop = (timeStamp: number) => {
     return
   }
   context.clearRect(0, 0, width, height)
-  // const currentlyPressedKey = pressedKey;
 
   document.onkeydown = globallyAssignPressedKey
   document.onkeyup = setArrowKeysNull
-  // else if (pressedKey === "p" && ) {
 
-  // }
-
-  // else {
-  //   document.onkeydown = globallyAssignPressedKey;
-  // }
   gameUpdate(deltaTime)
   draw()
   window.requestAnimationFrame(gameLoop)
@@ -103,13 +158,24 @@ const gameUpdate = (deltaTime: number) => {
   if (paused) {
     return
   }
+
+  // handle key inputs
   if (pressedKey === 'ArrowRight' && player.x < width - 30 - player.width) {
-    player.x += 10
-    // player.x += player.speed * deltaTime;
-  }
-  if (pressedKey === 'ArrowLeft' && player.x > 30) {
+    player.x += player.speed * deltaTime
+  } else if (pressedKey === 'ArrowLeft' && player.x > 30) {
     player.x -= player.speed * deltaTime
+  } else if (pressedKey === ' ') {
+    console.log('space pressed -- fire shots')
+    addMissileToPlayer()
   }
+
+  // TODO advance any missiles that exist in the array Missiles
+  if (player.missiles.length > 0) {
+    player.missiles.forEach((missile) => {
+      missile.y -= 1
+    })
+  }
+
   // if (player.x < width - player.width) {
   //   player.x += 1;
   // }
